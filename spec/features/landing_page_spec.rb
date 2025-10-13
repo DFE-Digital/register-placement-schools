3c2baeaf6d1c51a1720bbb15db86fe08c607fed9
defong <470137+defong@users.noreply.github.com>
2025-10-13 16:04:01 +0100

    [0256] Added specs for start page

---
+++ spec/features/landing_page_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "landing page" do
+  scenario "navigate to start" do
+    given_i_am_on_the_start_page
+    and_i_should_see_the_service_name
+    and_i_should_see_the_phase_banner
+    when_i_click_on("Sign in")
+    then_i_am_taken_to("#")
+  end
+
+  def given_i_am_on_the_start_page
+    visit "/"
+  end
+
+  def when_i_click_on_sign_in
+    page.click_on("Sign in")
+  end
+
+  def and_i_should_see_the_service_name
+    expect(page).to have_heading("h1", "Register of placement schools")
+    expect(page).to have_heading("h2", "Before you start")
+
+    expect(page).to have_title("Register of placement schools - GOV.UK")
+  end
+
+  def and_i_should_see_the_phase_banner
+    expect(page).to have_phase_banner
+  end
+end
