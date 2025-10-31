34a3313d9cd0e6a05555585b1f59fe9e20ed180f
defong <470137+defong@users.noreply.github.com>
2025-10-31 15:31:25 +0000

    [0257] Added user models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,139 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :uuid             not null, primary key
+#  dfe_sign_in_uid   :string
+#  discarded_at      :datetime
+#  email             :string           not null
+#  first_name        :string           not null
+#  last_name         :string           not null
+#  last_signed_in_at :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_discarded_at  (discarded_at)
+#  index_users_on_email         (email) UNIQUE
+#
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
+  it { is_expected.to be_audited }
+  it { is_expected.to be_kept }
+
+  context "user is discarded" do
+    before do
+      user.discard!
+    end
+
+    it "the user is discarded" do
+      expect(user).to be_discarded
+    end
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:first_name).with_message("Enter first name") }
+    it { is_expected.to validate_presence_of(:last_name).with_message("Enter last name") }
+    it { is_expected.to validate_presence_of(:email).with_message("Enter email address") }
+
+    context "when email is unique" do
+      it "is valid with a unique email" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "when email is not unique" do
+      let(:another_user) { build(:user, email: user.email) }
+
+      it "is not valid with a duplicate email" do
+        expect(another_user).not_to be_valid
+        expect(another_user.errors[:email]).to include("Email address already in use")
+      end
+    end
+
+    context "when email format is valid" do
+      let(:user) { build(:user, email: "email@#{hostname}") }
+
+      ["education.gov.uk", "good.education.gov.uk"].each do |hostname|
+        context "valid hostname" do
+          let(:hostname) { hostname }
+          it "is valid with a valid email format" do
+            expect(user).to be_valid
+          end
+        end
+      end
+
+      context "invalid hostname" do
+        let(:hostname) { "badeducation.gov.uk" }
+        it "is not valid email" do
+          expect(user).not_to be_valid
+          expect(user.errors[:email]).to include(
+            "Enter a Department for Education email address in the correct format, like name@education.gov.uk"
+          )
+        end
+      end
+    end
+
+    context "when email format is invalid" do
+      let(:user) { build(:user, email: "invalid") }
+
+      it "is not valid with an invalid email format" do
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include(
+          "Enter a Department for Education email address in the correct format, like name@education.gov.uk"
+        )
+      end
+    end
+  end
+
+  describe "scopes" do
+    describe ".order_by_first_then_last_name" do
+      let!(:albert_einstein)   { create(:user, first_name: "Albert", last_name: "Einstein") }
+      let!(:marie_curie)       { create(:user, first_name: "Marie", last_name: "Curie") }
+      let!(:isaac_newton)      { create(:user, first_name: "Isaac", last_name: "Newton") }
+      let!(:albert_schweitzer) { create(:user, first_name: "Albert", last_name: "Schweitzer") }
+
+      subject { described_class.order_by_first_then_last_name }
+
+      it "returns users ordered by first_name, then last_name ascending" do
+        expect(subject).to eq([
+          albert_einstein,
+          albert_schweitzer,
+          isaac_newton,
+          marie_curie
+        ])
+      end
+    end
+  end
+
+  describe "#name" do
+    let(:user) { create(:user, first_name: "John", last_name: "Doe") }
+
+    it "returns full name when unchanged" do
+      expect(user.name).to eq "John Doe"
+    end
+
+    it "returns original name when changed but not saved" do
+      user.first_name = "Jane"
+      expect(user.name).to eq "John Doe"
+
+      user.last_name = "Smith"
+      expect(user.name).to eq "John Doe"
+    end
+
+    it "returns new name after saving" do
+      user.update!(first_name: "Jane", last_name: "Smith")
+      expect(user.name).to eq "Jane Smith"
+    end
+
+    it "handles new records with changes" do
+      new_user = build(:user, first_name: "Alice", last_name: "Wonder")
+      new_user.first_name = "Alicia"
+      expect(new_user.name).to eq "Alicia Wonder"
+    end
+  end
+end
