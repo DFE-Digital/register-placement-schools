85a8bc1d05d2badfc8651214d29fedb3a951e878
defong <470137+defong@users.noreply.github.com>
2025-10-31 15:32:43 +0000

    [0257] Added rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require "rspec/rails"
+require "audited-rspec"
+require "capybara-screenshot/rspec"
+require "pundit/rspec"
 
 Rails.root.glob("spec/support/**/*.rb").each { |f| require f }
 
